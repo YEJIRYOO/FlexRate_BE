2ff01a5ca99c3e92ccddcfae5bb9a2099c2bab85
YEJIRYOO <110809579+YEJIRYOO@users.noreply.github.com>
2023-11-13 19:08:19 +0900

    백업3

--- repository/LoanRepository.java
+++ repository/LoanRepository.java
@@ -4,6 +4,8 @@ import com.sbb.flexrate.domain.Credit;
 import com.sbb.flexrate.domain.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LoanRepository extends JpaRepository<Loan,Long> {
-    Loan findByCreditId(Long creditId);//사용자 아이디 기반 credit 정보 조회
+    Optional<Loan> findByMemberId(Long memberId);//사용자 아이디 기반 credit 정보 조회
 }
